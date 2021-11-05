38d56b975d168e439894a4ed8b98a67253d12c8a
AzureSDKPipelineBot <sdkautomationpipeline@outlook.com>
2021-11-05 09:52:56 +0000

    autogenerated

---
+++ sdk/healthcareapis/Azure.ResourceManager.Healthcareapis/tests/HealthcareapisManagementTestEnvironment.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+
+namespace Azure.ResourceManager.Healthcareapis.Tests
+{
+    public class HealthcareapisManagementTestEnvironment : TestEnvironment
+    {
+    }
+}
