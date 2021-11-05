306115737b2dce23f3042c07ba41297110c55557
AzureSDKPipelineBot <sdkautomationpipeline@outlook.com>
2021-11-05 10:29:13 +0000

    autogenerated

---
+++ sdk/webpubsub/Azure.ResourceManager.Webpubsub/tests/WebpubsubManagementTestEnvironment.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+
+namespace Azure.ResourceManager.Webpubsub.Tests
+{
+    public class WebpubsubManagementTestEnvironment : TestEnvironment
+    {
+    }
+}
